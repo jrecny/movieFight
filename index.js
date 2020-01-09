a7abbed0872d94787e64a8e5008cccfa8e884262
John Recny <jrecny@outlook.com>
2020-01-09 14:05:30 -0500

    create utils

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params : {
 			apikey : '187db6ca',
-			i      : 'tt0848228'
+			s      : searchTerm
 		}
 	});
 
-	console.log(response.data);
-};
+	if (response.data.Error) {
+		return [];
+	}
 
-fetchData();
+	return response.data.Search;
+};
+const input = document.querySelector('input');
+
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+
+	for (let movie of movies) {
+		const div = document.createElement('div');
+		div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+
+		document.querySelector('#target').appendChild(div);
+	}
+};
+input.addEventListener('input', debounce(onInput, 500));
